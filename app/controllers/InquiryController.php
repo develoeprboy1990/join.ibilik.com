2abeabc4b63e3d53c6be037c59d87d99357b43b0
Mutahir Shah <mutahirshah123@gmail.com>
2023-12-06 11:47:13 +0500

    shared controller updated

--- app/controllers/InquiryController.php
+++ app/controllers/InquiryController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
 
@@ -8,29 +8,31 @@
 
  */
 
-class InquiryController extends SecureController{
+class InquiryController extends SecureController
+{
 
-	function __construct(){
+	function __construct()
+	{
 
 		parent::__construct();
 
 		$this->tablename = "inquiry";
-
 	}
 
 	/**
 
-     * List page records
+	 * List page records
 
-     * @param $fieldname (filter record by a field) 
+	 * @param $fieldname (filter record by a field) 
 
-     * @param $fieldvalue (filter field value)
+	 * @param $fieldvalue (filter field value)
 
-     * @return BaseView
+	 * @return BaseView
 
-     */
+	 */
 
-	function index($fieldname = null , $fieldvalue = null){
+	function index($fieldname = null, $fieldvalue = null)
+	{
 
 		$request = $this->request;
 
@@ -38,31 +40,31 @@ class InquiryController extends SecureController{
 
 		$tablename = $this->tablename;
 
-		$fields = array("id", 
+		$fields = array(
+			"id",
 
-			"prospect_name", 
+			"prospect_name",
 
-			"prospect_phone", 
+			"prospect_phone",
 
-			"select_location", 
+			"select_location",
 
-			"platform", 
+			"budget_rental",
+			"platform",
 
-			"datetime", 
+			"datetime",
 
-			"assign_agent_name", 
-
-			"remark", 
-
-			"handled");
+			"assign_agent_name",
+			"assign_agent_phone"
+		);
 
 		$pagination = $this->get_pagination(MAX_RECORD_COUNT); // get current pagination e.g array(page_number, page_limit)
 
 		//search table record
 
-		if(!empty($request->search)){
+		if (!empty($request->search)) {
 
-			$text = trim($request->search); 
+			$text = trim($request->search);
 
 			$search_condition = "(
 
@@ -90,7 +92,7 @@ class InquiryController extends SecureController{
 
 			$search_params = array(
 
-				"%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%"
+				"%$text%", "%$text%", "%$text%", "%$text%", "%$text%", "%$text%", "%$text%", "%$text%", "%$text%", "%$text%"
 
 			);
 
@@ -98,107 +100,73 @@ class InquiryController extends SecureController{
 
 			$db->where($search_condition, $search_params);
 
-			 //template to use when ajax search
+			//template to use when ajax search
 
 			$this->view->search_template = "inquiry/search.php";
-
 		}
 
-		if(!empty($request->orderby)){
+		if (!empty($request->orderby)) {
 
 			$orderby = $request->orderby;
 
 			$ordertype = (!empty($request->ordertype) ? $request->ordertype : ORDER_TYPE);
 
 			$db->orderBy($orderby, $ordertype);
-
-		}
-
-		else{
+		} else {
 
 			$db->orderBy("inquiry.id", ORDER_TYPE);
-
 		}
 
-		$allowed_roles = array ('administrator');
+		$allowed_roles = array('administrator');
 
-		if(!in_array(strtolower(USER_ROLE), $allowed_roles)){
-
-		$db->where("inquiry.assign_agent_name", get_active_user('agent_name') );
-
+		if (!in_array(strtolower(USER_ROLE), $allowed_roles)) {
+			$db->where("inquiry.assign_agent_name", get_active_user('agent_name'));
+		}
+		if ($fieldname) {
+			$db->where($fieldname, $fieldvalue); //filter by a single field name
 		}
 
-		if($fieldname){
-
-			$db->where($fieldname , $fieldvalue); //filter by a single field name
-
-		}
-
-		if(!empty($request->inquiry_handled)){
-
+		if (!empty($request->inquiry_handled)) {
 			$val = $request->inquiry_handled;
-
-			$db->where("inquiry.handled", $val , "=");
-
+			$db->where("inquiry.handled", $val, "=");
 		}
-
 		$tc = $db->withTotalCount();
-
 		$records = $db->get($tablename, $pagination, $fields);
-
 		$records_count = count($records);
-
 		$total_records = intval($tc->totalCount);
-
 		$page_limit = $pagination[1];
-
 		$total_pages = ceil($total_records / $page_limit);
-
 		$data = new stdClass;
-
 		$data->records = $records;
-
 		$data->record_count = $records_count;
-
 		$data->total_records = $total_records;
-
 		$data->total_page = $total_pages;
-
-		if($db->getLastError()){
-
+		if ($db->getLastError()) {
 			$this->set_page_error();
-
 		}
-
 		$page_title = $this->view->page_title = "Inquiry";
-
 		$this->view->report_filename = date('Y-m-d') . '-' . $page_title;
-
 		$this->view->report_title = $page_title;
-
 		$this->view->report_layout = "report_layout.php";
-
 		$this->view->report_paper_size = "A4";
-
 		$this->view->report_orientation = "portrait";
-
 		$this->render_view("inquiry/list.php", $data); //render the full page
-
 	}
 
 	/**
 
-     * View record detail 
+	 * View record detail 
 
 	 * @param $rec_id (select record by table primary key) 
 
-     * @param $value value (select record by value of field name(rec_id))
+	 * @param $value value (select record by value of field name(rec_id))
 
-     * @return BaseView
+	 * @return BaseView
 
-     */
+	 */
 
-	function view($rec_id = null, $value = null){
+	function view($rec_id = null, $value = null)
+	{
 
 		$request = $this->request;
 
@@ -208,98 +176,89 @@ class InquiryController extends SecureController{
 
 		$tablename = $this->tablename;
 
-		$fields = array("id", 
+		$fields = array(
+			"id",
 
-			"prospect_name", 
+			"prospect_name",
 
-			"prospect_phone", 
+			"prospect_phone",
 
-			"select_location", 
+			"select_location",
 
-			"platform", 
+			"platform",
 
-			"datetime", 
+			"datetime",
 
-			"assign_agent_name", 
+			"assign_agent_name",
 
-			"assign_agent_phone", 
+			"assign_agent_phone",
 
-			"remark", 
+			"remark",
 
-			"handled");
+			"budget_rental"
+		);
 
-		$allowed_roles = array ('administrator');
+		$allowed_roles = array('administrator');
 
-		if(!in_array(strtolower(USER_ROLE), $allowed_roles)){
+		if (!in_array(strtolower(USER_ROLE), $allowed_roles)) {
 
-		$db->where("inquiry.assign_agent_name", get_active_user('agent_name') );
-
+			$db->where("inquiry.assign_agent_name", get_active_user('agent_name'));
 		}
 
-		if($value){
+		if ($value) {
 
 			$db->where($rec_id, urldecode($value)); //select record based on field name
 
-		}
-
-		else{
+		} else {
 
 			$db->where("inquiry.id", $rec_id);; //select record based on primary key
 
 		}
 
-		$record = $db->getOne($tablename, $fields );
+		$record = $db->getOne($tablename, $fields);
 
-		if($record){
+		if ($record) {
 
 			$page_title = $this->view->page_title = "View  Inquiry";
 
-		$this->view->report_filename = date('Y-m-d') . '-' . $page_title;
+			$this->view->report_filename = date('Y-m-d') . '-' . $page_title;
 
-		$this->view->report_title = $page_title;
+			$this->view->report_title = $page_title;
 
-		$this->view->report_layout = "report_layout.php";
+			$this->view->report_layout = "report_layout.php";
 
-		$this->view->report_paper_size = "A4";
+			$this->view->report_paper_size = "A4";
 
-		$this->view->report_orientation = "portrait";
+			$this->view->report_orientation = "portrait";
+		} else {
 
-		}
-
-		else{
-
-			if($db->getLastError()){
+			if ($db->getLastError()) {
 
 				$this->set_page_error();
-
-			}
-
-			else{
+			} else {
 
 				$this->set_page_error("No record found");
-
 			}
-
 		}
 
 		return $this->render_view("inquiry/view.php", $record);
-
 	}
 
 	/**
 
-     * Insert new record to the database table
+	 * Insert new record to the database table
 
 	 * @param $formdata array() from $_POST
 
-     * @return BaseView
+	 * @return BaseView
 
-     */
+	 */
 
-	function add($formdata = null){
+	function add($formdata = null)
+	{
 
-		if($formdata){
-			
+		if ($formdata) {
+
 			$db = $this->GetModel();
 
 			$tablename = $this->tablename;
@@ -308,18 +267,13 @@ class InquiryController extends SecureController{
 
 			//fillable fields
 
-			$fields = $this->fields = array("prospect_name","prospect_phone","select_location","platform","datetime","assign_agent_name","assign_agent_phone","remark","handled");
+			$fields = $this->fields = array("prospect_name", "prospect_phone", "select_location", "platform", "datetime", "assign_agent_name", "assign_agent_phone", "remark", "budget_rental");
 
 			$postdata = $this->format_request_data($formdata);
 
 			$this->rules_array = array(
-
 				'prospect_name' => 'required',
-
 				'prospect_phone' => 'required',
-
-				'datetime' => 'required',
-
 				'assign_agent_name' => 'required',
 
 			);
@@ -334,80 +288,59 @@ class InquiryController extends SecureController{
 
 				'platform' => 'sanitize_string',
 
-				'datetime' => 'sanitize_string',
-
 				'assign_agent_name' => 'sanitize_string',
 
 				'assign_agent_phone' => 'sanitize_string',
-				'budget_rental'=>'sanitize_string',
-
-				'remark' => 'sanitize_string',
-
-				'handled' => 'sanitize_string',
+				'budget_rental' => 'sanitize_string'
 
 			);
-
 			$this->filter_vals = true; //set whether to remove empty fields
-
 			$modeldata = $this->modeldata = $this->validate_form($postdata);
+			if (1) {
+				# Statement to execute before adding record
+				$location = $modeldata['select_location'];
+				$agentcount = $db->rawQueryValue("SELECT COUNT(id) FROM agent WHERE agent_location = ? AND status1 = '0' LIMIT 1", array($location));
 
-			if(1){
+				if ($agentcount == 0) {
+					$db->rawQuery("UPDATE agent SET status1 = '0' WHERE agent_location = ?", array($location));
+				}
+				$agentid                         = $db->rawQueryValue("SELECT id FROM agent WHERE agent_location = '$location' and status1 = '0' limit 1");
 
-		# Statement to execute before adding record
+				if (empty($modeldata['assign_agent_name'])) {
+					$agentname                       = $db->rawQueryValue("SELECT agent_name FROM agent WHERE agent_location = '$location' and status1 = '0' limit 1");
+					$modeldata['assign_agent_name']  = $agentname;
+				} else {
+					$agentname = $modeldata['assign_agent_name'];
+				}
+				if (empty($modeldata['assign_agent_phone'])) {
+					$agentphone                      = $this->formatPhone($db->rawQueryValue("SELECT agent_phone FROM agent WHERE agent_location = '$location' and status1 = '0' limit 1"));
+					$modeldata['assign_agent_phone'] = $agentphone;
+				} else {
+					$agentphone = $modeldata['assign_agent_phone'];
+				}
 
-		        $cname  = $modeldata['prospect_name'];
+				//$modeldata['agency']             = $agency;
+				date_default_timezone_set("Asia/Kuala_Lumpur");
+				$datetime = date('Y-m-d H:i:s'); //Returns IST
+				$modeldata['datetime'] = $datetime;
+				$db->rawQuery("UPDATE agent SET status1 ='1' WHERE id='$agentid'");
+				# End of before add statement
+				$rec_id = $this->rec_id = $db->insert($tablename, $modeldata);
 
-                $cphone = $this->formatPhone($modeldata['prospect_phone']);
-
-                $location = $modeldata['select_location'];
-
-                $agentcount = $db->rawQueryValue("SELECT COUNT(id) FROM agent WHERE agent_location = ? AND status1 = '0' LIMIT 1", array($location));
-
-                if ($agentcount == 0) {
-
-                    $db->rawQuery("UPDATE agent SET status1 = '0' WHERE agent_location = ?", array($location));
-
-                }
-
-                $agentname                       = $db->rawQueryValue("SELECT agent_name FROM agent WHERE agent_location = '$location' and status1 = '0' limit 1");
-
-                $agentphone                      = $this->formatPhone($db->rawQueryValue("SELECT agent_phone FROM agent WHERE agent_location = '$location' and status1 = '0' limit 1"));
-
-                 //$agency = $db->rawQueryValue("SELECT agency_name FROM agent WHERE agent_name = '$agentname'");
-
-                $agentid                         = $db->rawQueryValue("SELECT id FROM agent WHERE agent_location = '$location' and status1 = '0' limit 1");
-
-                $modeldata['assign_agent_name']  = $agentname;
-
-                $modeldata['assign_agent_phone'] = $agentphone;
-
-                //$modeldata['agency']             = $agency;
-
-                date_default_timezone_set("Asia/Kuala_Lumpur");
-
-                $datetime = date('Y-m-d H:i:s'); //Returns IST
-
-                $modeldata['datetime'] = $datetime;
-
-                $db->rawQuery("UPDATE agent SET status1 ='1' WHERE id='$agentid'");
-
-		        # End of before add statement
-
-					$rec_id = $this->rec_id = $db->insert($tablename, $modeldata);
 				if ($rec_id) {
-					//$this->set_flash_msg("Record added successfully", "success");
-					header("Location: https://api.whatsapp.com/send?phone=$agentphone&text=hi $agentname, I would like to find a room at $location, Please show me your room catalogue, thanks");
-					exit; 
+					if ($_SERVER['HTTP_REFERER'] == SITE_ADDR . 'inquiries/welcome.php') {
+						header("Location: https://api.whatsapp.com/send?phone=$agentphone&text=hi $agentname, I would like to find a room at $location, Please show me your room catalogue, thanks");
+						exit;
+					}
+					$this->set_flash_msg("Record added successfully", "success");
+					return	$this->redirect("inquiry");
 				} else {
 					$this->set_page_error();
 				}
-
 			}
-
 		}
 		$page_title = $this->view->page_title = "Add New Inquiry";
 		$this->render_view("inquiry/add.php");
-
 	}
 
 
@@ -425,13 +358,10 @@ class InquiryController extends SecureController{
 		if (preg_match('/^601/ ', $phoneNumber) === 1) {
 
 			return $phoneNumber;
-
 		} else if (preg_match('/^01/ ', $phoneNumber) === 1) {
 
 			return '6' . $phoneNumber;
-
 		}
-
 	}
 
 
@@ -445,22 +375,22 @@ class InquiryController extends SecureController{
 		$select_location_options = $comp_model->inquiry_select_location_option_list();
 
 		echo json_encode($select_location_options);
-
 	}
 
 	/**
 
-     * Update table record with formdata
+	 * Update table record with formdata
 
 	 * @param $rec_id (select record by table primary key)
 
 	 * @param $formdata array() from $_POST
 
-     * @return array
+	 * @return array
 
-     */
+	 */
 
-	function edit($rec_id = null, $formdata = null){
+	function edit($rec_id = null, $formdata = null)
+	{
 
 		$request = $this->request;
 
@@ -470,11 +400,11 @@ class InquiryController extends SecureController{
 
 		$tablename = $this->tablename;
 
-		 //editable fields
+		//editable fields
 
-		$fields = $this->fields = array("id","prospect_name","prospect_phone","select_location","platform","remark","handled");
+		$fields = $this->fields = array("id", "prospect_name", "prospect_phone", "select_location", "platform", "remark", "budget_rental");
 
-		if($formdata){
+		if ($formdata) {
 
 			$postdata = $this->format_request_data($formdata);
 
@@ -503,65 +433,36 @@ class InquiryController extends SecureController{
 			);
 
 			$modeldata = $this->modeldata = $this->validate_form($postdata);
-
-			if($this->validated()){
-
-		$allowed_roles = array ('administrator');
-
-		if(!in_array(strtolower(USER_ROLE), $allowed_roles)){
-
-		$db->where("inquiry.assign_agent_name", get_active_user('agent_name') );
-
-		}
-
+			if ($this->validated()) {
+				$allowed_roles = array('administrator');
+				if (!in_array(strtolower(USER_ROLE), $allowed_roles)) {
+					$db->where("inquiry.assign_agent_name", get_active_user('agent_name'));
+				}
 				$db->where("inquiry.id", $rec_id);;
-
 				$bool = $db->update($tablename, $modeldata);
-
 				$numRows = $db->getRowCount(); //number of affected rows. 0 = no record field updated
-
-				if($bool && $numRows){
-
+				if ($bool && $numRows) {
 					$this->set_flash_msg("Yeah! Record updated successfully", "success");
-
 					return $this->redirect("inquiry");
-
-				}
-
-				else{
-
-					if($db->getLastError()){
-
+				} else {
+					if ($db->getLastError()) {
 						$this->set_page_error();
-
-					}
-
-					elseif(!$numRows){
-
+					} elseif (!$numRows) {
 						//not an error, but no record was updated
-
 						$page_error = "No record updated";
-
 						$this->set_page_error($page_error);
-
 						$this->set_flash_msg($page_error, "warning");
-
 						return	$this->redirect("inquiry");
-
 					}
-
 				}
-
 			}
-
 		}
 
-		$allowed_roles = array ('administrator');
+		$allowed_roles = array('administrator');
 
-		if(!in_array(strtolower(USER_ROLE), $allowed_roles)){
+		if (!in_array(strtolower(USER_ROLE), $allowed_roles)) {
 
-		$db->where("inquiry.assign_agent_name", get_active_user('agent_name') );
-
+			$db->where("inquiry.assign_agent_name", get_active_user('agent_name'));
 		}
 
 		$db->where("inquiry.id", $rec_id);;
@@ -570,27 +471,26 @@ class InquiryController extends SecureController{
 
 		$page_title = $this->view->page_title = "Edit  Inquiry";
 
-		if(!$data){
+		if (!$data) {
 
 			$this->set_page_error();
-
 		}
 
 		return $this->render_view("inquiry/edit.php", $data);
-
 	}
 
 	/**
 
-     * Delete record from the database
+	 * Delete record from the database
 
 	 * Support multi delete by separating record id by comma.
 
-     * @return BaseView
+	 * @return BaseView
 
-     */
+	 */
 
-	function delete($rec_id = null){
+	function delete($rec_id = null)
+	{
 
 		Csrf::cross_check();
 
@@ -608,33 +508,25 @@ class InquiryController extends SecureController{
 
 		$db->where("inquiry.id", $arr_rec_id, "in");
 
-		$allowed_roles = array ('administrator');
+		$allowed_roles = array('administrator');
 
-		if(!in_array(strtolower(USER_ROLE), $allowed_roles)){
+		if (!in_array(strtolower(USER_ROLE), $allowed_roles)) {
 
-		$db->where("inquiry.assign_agent_name", get_active_user('agent_name') );
-
+			$db->where("inquiry.assign_agent_name", get_active_user('agent_name'));
 		}
 
 		$bool = $db->delete($tablename);
 
-		if($bool){
+		if ($bool) {
 
 			$this->set_flash_msg("Record deleted successfully", "success");
-
-		}
-
-		elseif($db->getLastError()){
+		} elseif ($db->getLastError()) {
 
 			$page_error = $db->getLastError();
 
 			$this->set_flash_msg($page_error, "danger");
-
 		}
 
 		return	$this->redirect("inquiry");
-
 	}
-
 }
-
